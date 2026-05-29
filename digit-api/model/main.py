76ca15bfe1514cf9b3ec5b3130e4ad08f7d28dbc
mutassimalzeem <mutassimalshahriar@gmail.com>
2026-05-29 22:46:03 +0600

    Deployment working

--- digit-api/model/main.py
+++ digit-api/model/main.py
@@ -2,20 +2,20 @@ from fastapi import FastAPI, UploadFile, File
 import joblib, io
 from pydantic import BaseModel
 import numpy as np
+from PIL import Image
 
 
 model = joblib.load('digit-api\\model\\digit_model.h5')
 app = FastAPI()
 
 
-class ModelInput(BaseModel):
-    pass
 
 @app.get('/')
 async def root():
     return {"message": 'Hello World'}
 
 @app.post('/predict')
-async def predict(data: ModelInput):
-    input_df = 
-    prediction = model.predict(input_df)
+async def predict(file: UploadFile = File(...)):
+    input_img = await file.read()
+    image = Image.open 
+    prediction = model.predict(input_data)
